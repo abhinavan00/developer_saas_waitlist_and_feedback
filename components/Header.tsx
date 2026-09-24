9230aa01c97c5330b53c3491f25daf0798a1b0c0
Abhinav Anand <abhinavanand072@gmail.com>
2026-09-24 12:28:50 +0530

    Header component complete

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,25 @@
-import BrandLogo from '@/public/brand-logo.svg'; 
+import Image from 'next/image';
+import mobileLogo from '@/public/logo-mobile.svg'; 
 
 export default function Header() {
     return (
-        <div>
-            
+        <div 
+            className={`
+                bg-[#121315]/80 backdrop-blur-xl drop-shadow-sm 
+                flex justify-between 
+                border-b border-[#3B4B3D]/30 p-4
+            `}
+        >
+            <Image src={mobileLogo} alt='Logo' />
+            <button 
+                className={`
+                    bg-[#10F07A] text-[#006830] text-sm py-2 px-4 
+                    rounded-lg drop-shadow-2xl cursor-pointer 
+                    hover:bg-[#63FF94] hover:drop-shadow-lg hover:drop-shadow-[#63FF94]/20
+                `}
+            >
+                Feedback Board
+            </button>
         </div>
     )
 }
