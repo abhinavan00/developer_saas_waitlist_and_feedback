92defc0b91b65c0acc4cb1fb0b0ba99adb271ceb
Abhinav Anand <abhinavanand072@gmail.com>
2026-09-24 13:05:33 +0530

    Added responsiveness to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,17 +5,18 @@ export default function Header() {
     return (
         <div 
             className={`
-                bg-[#121315]/80 backdrop-blur-xl drop-shadow-sm 
+                bg-[#121315]/80 backdrop-blur-xl drop-shadow-sm drop-shadow-[#000000]/40
                 flex justify-between 
                 border-b border-[#3B4B3D]/30 p-4
             `}
         >
-            <Image src={mobileLogo} alt='Logo' />
+            <Image className={`md:w-40`} src={mobileLogo} alt='Logo' />
             <button 
                 className={`
-                    bg-[#10F07A] text-[#006830] text-sm py-2 px-4 
-                    rounded-lg drop-shadow-2xl cursor-pointer 
+                    bg-[#10F07A] text-[#006830] text-sm font-mono font-medium 
+                    py-2 px-4 rounded-lg drop-shadow-2xl cursor-pointer 
                     hover:bg-[#63FF94] hover:drop-shadow-lg hover:drop-shadow-[#63FF94]/20
+                    md:text-lg
                 `}
             >
                 Feedback Board
