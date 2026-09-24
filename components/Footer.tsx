00be3544e07b6890c372b45a5617b0fd550a4926
Abhinav Anand <abhinavanand072@gmail.com>
2026-09-24 12:51:47 +0530

    footer component complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image';
+import mobileLogo from '@/public/logo-mobile.svg';
+
 export default function Footer() {
     return (
-        <div>
-            
+        <div
+            className={`
+                bg-[#0D0E10] py-6 px-4   
+            `}
+        >
+            <div className={`flex justify-between`}>
+                <Image src={mobileLogo} alt='logo' />
+                <p 
+                className={`
+                    font-mono text-[#BACBB9] text-xs bg-[#1B1C1E] px-4 rounded-2xl
+                    flex justify-center items-center gap-2
+                `}
+                >
+                    <span><div className='w-3 h-3 bg-[#10F07A] rounded-4xl'></div></span> 
+                    All systems operational
+                </p>
+            </div> 
+            <p className={`font-mono text-[#BACBB9] text-xs text-center mt-8`}>
+                © 2026 DevPulse Inc. All rights reserved.
+            </p>   
         </div>
     )
 }
